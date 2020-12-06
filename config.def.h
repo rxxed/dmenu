84d3b869ba04efaded58639510b80d9010f0449e
Raaed Shaikh <rd5k@tutanota.de>
2020-12-06 17:30:40 +0530

    removed center and border patches

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-        "Terminus (TTF):pixelsize=18:antialias=true:autohint=true",
+        "Terminus (TTF):pixelsize=16:antialias=true:autohint=true",
         "JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -12,10 +12,10 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#000000" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut] = { "#000000", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
